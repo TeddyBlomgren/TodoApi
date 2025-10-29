cbd5308872d18ad4897d32575c477ece9d94e6df
Noel Burman <noelburman@hotmail.com>
2025-10-29 13:31:31 +0100

    add Models folder with ToDoItems and Data folder with DbContext

---
+++ TodoApi/Data/DbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+    }
+}
---
+++ TodoApi/Models/ToDoItems.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoApi.Models
+{
+    public class ToDoItems
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }   
+    }
+}
