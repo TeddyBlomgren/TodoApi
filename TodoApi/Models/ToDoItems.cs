1f51cd37787d9dabfdf6462a8c725bb8106d236d
Noel Burman <noelburman@hotmail.com>
2025-11-03 09:42:39 +0100

    Add required and MaxLength to Title and Description

--- TodoApi/Models/ToDoItems.cs
+++ TodoApi/Models/ToDoItems.cs
@@ -1,11 +1,19 @@
-﻿namespace TodoApi.Models
+﻿using Newtonsoft.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models
 {
     public class ToDoItems
     {
         public int Id { get; set; }
 
+        
+        [MaxLength(25, ErrorMessage = "Max Length is 25 numbers!!")]
+        [Required]
         public string Title { get; set; }
 
+        [Required]
+        [MaxLength(100, ErrorMessage ="Max Length is 100 numbers!!")]
         public string Description { get; set; }
 
         public bool IsCompleted { get; set; }   
